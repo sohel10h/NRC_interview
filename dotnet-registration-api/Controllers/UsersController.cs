f0360d52cda36aea166990b395a743b4cacc7066
SOHEL RANA <sohel10h@gmail.com>
2024-06-27 22:42:27 +0600

    init

---
+++ dotnet-registration-api/Controllers/UsersController.cs
@@ -0,0 +1,80 @@
+﻿using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Helpers;
+using dotnet_registration_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_registration_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UsersController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login([FromBody]LoginRequest model)
+        {
+            throw new NotImplementedException();
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody]RegisterRequest model)
+        {
+            if (string.IsNullOrEmpty(model.Username) && string.IsNullOrEmpty(model.Password)) 
+            {
+                return BadRequest();
+            }
+            var user = await _userService.Register(model);
+            return Ok(user);
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            var users= await _userService.GetAll();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+           var user= await _userService.GetById(id);
+            if (user is null) 
+            {
+                return NotFound();
+            }
+           return Ok(user);
+
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<User>> Update(int id, [FromBody]UpdateRequest model)
+        {
+            var user = await _userService.GetById(id);
+            if (user is null) 
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                return Ok(await _userService.Update(id, model));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var user = await _userService.GetById(id);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            await _userService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ dotnet-registration-api/Services/UserService.cs
@@ -0,0 +1,62 @@
+﻿using dotnet_registration_api.Data.Entities;
+using dotnet_registration_api.Data.Models;
+using dotnet_registration_api.Data.Repositories;
+using dotnet_registration_api.Helpers;
+using Mapster;
+
+namespace dotnet_registration_api.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+        public UserService(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<List<User>> GetAll()
+        {
+            return await _userRepository.GetAllUsers();
+        }
+        public async Task<User> GetById(int id)
+        {
+          return  await _userRepository.GetUserById(id);
+        }
+        public async Task<User> Login(LoginRequest login)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<User> Register(RegisterRequest register)
+        {
+          var user = await  _userRepository.CreateUser(
+             new User
+             {
+                FirstName = register.FirstName,
+                LastName = register.LastName,
+                Username = register.Username,
+                PasswordHash = HashHelper.HashPassword(register.Password)
+            });
+            return user;
+        }
+        public async Task<User> Update(int id, UpdateRequest updateRequest)
+        {
+            var user = await _userRepository.GetUserByUsernameAndPassword(updateRequest.Username, HashHelper.HashPassword(updateRequest.OldPassword));
+            if (user == null) 
+            {
+                throw new NotFoundException();
+            }
+            return await _userRepository.UpdateUser(new User
+            {
+                FirstName = updateRequest.FirstName,
+                LastName = updateRequest.LastName,
+                Username = updateRequest.Username,
+                PasswordHash = HashHelper.HashPassword(updateRequest.NewPassword)
+            }); 
+        }
+        public async Task Delete(int id)
+        {
+
+           await _userRepository.DeleteUser(id);
+        }
+
+    }
+}
